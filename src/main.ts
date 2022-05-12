e656a6b6bdc1554ab8b8d3f397ba60b53f41d444
arsal <arsal.hameed@gmail.com>
2022-05-12 16:19:19 +0400

    change server port and host made it configuration driven also setup swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = app.get(ConfigService);
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Social Feed')
+    .setDescription('The con cruise API description')
+    .setVersion('1.0')
+    .addServer(config.get('app.SERVER'))
+    .addTag('social-feed')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('documentation', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  await app.listen(config.get('app.PORT'), config.get('app.HOST'));
 }
 bootstrap();
