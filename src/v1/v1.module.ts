229a1959e00e0fe9d47e19a0c2c683f9d1eb2459
arsal <arsal.hameed@gmail.com>
2022-05-12 19:46:54 +0400

    v1 module added post

--- src/v1/v1.module.ts
+++ src/v1/v1.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CustomerModule } from '@/v1/customer/customer.module';
 import { CategoriesModule } from "@/v1/categories/categories.module";
+import { PostModule } from "@/v1/post/post.module";
 
 @Module({
-  imports: [CustomerModule, CategoriesModule],
+  imports: [CustomerModule, CategoriesModule, PostModule],
   providers: [],
   exports: [],
 })
