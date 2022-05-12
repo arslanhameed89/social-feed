acdd54302c3b3b729b2b4fe66f847754c12cfed7
arsal <arsal.hameed@gmail.com>
2022-05-12 18:10:04 +0400

    provider extended

--- src/v1/customer/providers/customer.providers.ts
+++ src/v1/customer/providers/customer.providers.ts
@@ -1,11 +1,15 @@
 import { Connection } from 'mongoose'
 import { CustomerSchema } from '../schemas/customer.schema'
+import { CategorySchema } from "@/v1/categories/schemas/category.schema";
 
 
 export const CustomerProviders = [
   {
     provide: 'CUSTOMER_MODEL',
-    useFactory: (connection: Connection) : any => connection.model('customer', CustomerSchema, 'customers'),
+    useFactory: (connection: Connection) : any => {
+      if(!connection) return
+      return connection.model('customer', CategorySchema, 'customer')
+    },
     inject: ['MONGODB_PROVIDER'],
   }
 ];
