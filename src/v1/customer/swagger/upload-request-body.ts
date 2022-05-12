67314bfea8d593c88ae72ace84cfd66eadb9c9e9
arsal <arsal.hameed@gmail.com>
2022-05-12 21:58:16 +0400

    fields with file upload

---
+++ src/v1/customer/swagger/upload-request-body.ts
@@ -0,0 +1,17 @@
+export const UploadRequestBody = {
+  name: {
+    type: 'string'
+  },
+  password: {
+    type: 'string'
+  },
+  email: {
+    type: 'string'
+  },
+  mobile: {
+    type: 'number'
+  },
+  dateOfBirth: {
+    type: 'integer'
+  }
+}
