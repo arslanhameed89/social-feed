ac221ae643ce5e20a6ecd1d9ef8589e9641cb109
arsal <arsal.hameed@gmail.com>
2022-05-12 14:12:25 +0400

    db connection provider created

---
+++ providers/database.providers.ts
@@ -0,0 +1,54 @@
+import * as mongoose from 'mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const databaseProviders = [
+  {
+    provide: 'MONGODB_PROVIDER',
+    imports: [ConfigModule],
+    useFactory: async (
+      configService: ConfigService,
+    ): Promise<typeof mongoose> => {
+      let connection: mongoose.Mongoose = null;
+      //let retryCount = 0;
+      do {
+        try {
+          connection = await mongoose.connect(
+            configService.get<string>('APP.DB.MONGODB_URL'),
+          );
+          console.info(`connection has been made on first part`);
+        } catch (err) {
+          console.info(err);
+          //retryCount++;
+          //@TODO: add Logger
+        }
+      } while (connection === null);
+
+      mongoose.connection.on('connected', () => {
+        //@TODO: add Logger
+        console.info('connection has been made');
+      });
+
+      mongoose.connection.on('reconnected', () => {
+        //@TODO: add Logger
+        console.info('mongo reconnected');
+      });
+
+      mongoose.connection.on('disconnected', async () => {
+        connection = await mongoose.connect(
+          configService.get<string>('APP.DB.MONGODB_URL'),
+        );
+      });
+
+      mongoose.connection.on('close', () => {
+        //@TODO: add Logger;
+      });
+
+      mongoose.connection.on('error', () => {
+        //@TODO: add Logger
+      });
+
+      return connection;
+    },
+    inject: [ConfigService],
+  },
+];
