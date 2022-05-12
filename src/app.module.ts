3d1c47af3e800aee81f769da5dd11bf031d16b5a
arsal <arsal.hameed@gmail.com>
2022-05-12 16:13:36 +0400

    add core module, v1 module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import appConfig from "../config/app.config";
+import appConfig from "./config/app.config";
 import { ConfigModule } from "@nestjs/config";
-import { ProvidersModule } from "../providers/providers.module";
+import { ProvidersModule } from "./providers/providers.module";
+import { CoreModule } from "@/core/core.module";
+import { V1Module } from "@/v1/v1.module";
 
 @Module({
   imports: [
@@ -12,7 +14,9 @@ import { ProvidersModule } from "../providers/providers.module";
       load: [appConfig],
       envFilePath: ['.env'],
     }),
-    ProvidersModule
+    ProvidersModule,
+    CoreModule,
+    V1Module,
   ],
   controllers: [AppController],
   providers: [AppService],
