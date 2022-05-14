6a639522a2670c64ce861ebb8a24b26dca188300
arsal <arsal.hameed@gmail.com>
2022-05-14 13:00:48 +0400

    update category functionality

--- src/v1/categories/dto/update-category.dto.ts
+++ src/v1/categories/dto/update-category.dto.ts
@@ -1,12 +1,4 @@
-import { IsMongoId, IsNotEmpty } from "class-validator";
-import { ApiProperty, PartialType } from "@nestjs/swagger";
+import { PartialType } from "@nestjs/swagger";
 import { createCategoryDto } from "@/v1/categories/dto/create-category.dto";
 
-export class updateCategoryDto extends PartialType(createCategoryDto) {
-
-  @IsNotEmpty()
-  @ApiProperty()
-  @IsMongoId()
-  id: string;
-
-}
+export class updateCategoryDto extends PartialType(createCategoryDto) {}
