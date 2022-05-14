2a0cbd695a9f8f3ed19b500388462a77ff847cc8
arsal <arsal.hameed@gmail.com>
2022-05-14 13:23:21 +0400

    add upload functionality

--- src/v1/post/post.controller.ts
+++ src/v1/post/post.controller.ts
@@ -1,9 +1,16 @@
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UploadedFile } from "@nestjs/common";
 import { ApiTags } from '@nestjs/swagger';
 import { PostService } from './services/post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { Post as PostSchema } from './schemas/post.schema';
+import { ApiFile } from "@/core/decorators/api-file/api-file-decorator";
+import { UploadRequestBody } from "@/v1/post/swagger/upload-request-body";
+import { diskStorage } from "multer";
+import { Request } from "express";
+import { IFile } from "@/core/interfaces";
+import { randomUUID } from "crypto";
+import { fileMimetypeFilter } from "@/shared/helpers/file-mime-type-filter";
 
 @Controller('post')
 @ApiTags('post')
@@ -11,8 +18,18 @@ export class PostController {
   constructor(private customerService: PostService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto): Promise<PostSchema> {
-    return this.customerService.create(createPostDto);
+  @ApiFile('image', true, UploadRequestBody, {
+    storage: diskStorage({
+      destination: './uploads-tmp',
+      filename: (req: Request, file: IFile, callback: (error: Error | null, filename: string) => void) => {
+        callback(null, `${randomUUID()} - ${file.originalname}`);
+      }
+    }),
+    fileFilter: fileMimetypeFilter('image')
+  })
+  create(@Body() createPostDto: CreatePostDto,
+         @UploadedFile() file: Express.Multer.File): Promise<PostSchema> {
+    return this.customerService.create(createPostDto, file);
   }
 
   @Patch(':id')
