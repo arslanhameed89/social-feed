1b654e36594884fcb4cbff801a1ca73b38ad364b
arsal <arsal.hameed@gmail.com>
2022-05-14 15:10:23 +0400

    password hashing module adeed

---
+++ src/shared/hash/hash.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+
+
+@Injectable()
+export class HashService {
+  saltRounds: number;
+
+  constructor(
+    private configService: ConfigService,
+  ) {
+    this.saltRounds = this.configService.get<number>('APP.SALT_ROUND');
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    try {
+      return await bcrypt.hash(password, this.saltRounds);
+    } catch (err) {
+      console.info(err);
+      throw err;
+    }
+  }
+
+  async comparePassword(
+    password: string,
+    dbPassword: string,
+  ): Promise<boolean> {
+    try {
+      return await bcrypt.compare(password, dbPassword);
+    } catch (err) {
+      console.info(err);
+      throw err;
+    }
+  }
+}
