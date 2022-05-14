f158b975d82b432fe51a7fcde06e76b43a4bf6be
arsal <arsal.hameed@gmail.com>
2022-05-14 20:07:08 +0400

    extend service for db connection

--- src/shared/health/health.module.ts
+++ src/shared/health/health.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TerminusModule } from "@nestjs/terminus";
 import { HttpModule } from "@nestjs/axios";
 import { HealthCheckController } from "@/shared/health/health-check.controller";
+import { HealthService } from "@/shared/health/health.service";
 
 @Module({
   imports: [TerminusModule, HttpModule],
   controllers: [HealthCheckController],
+  providers: [HealthService],
   exports: []
 })
 
--- src/shared/health/health.service.ts
+++ src/shared/health/health.service.ts
@@ -1,5 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { HealthIndicator } from "@nestjs/terminus";
+import {
+  HealthCheckError,
+  HealthIndicator,
+  HealthIndicatorResult,
+} from "@nestjs/terminus";
+import * as mongoose from "mongoose";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
-export class HealthService extends HealthIndicator {}
+export class HealthService extends HealthIndicator {
+
+  constructor(
+    private readonly configService: ConfigService
+  ) {
+    super();
+  }
+
+  async isDbHealthy(key: string): Promise<HealthIndicatorResult> {
+    let isHealthy: boolean;
+    let connection: mongoose.Mongoose;
+    try {
+      const url = this.configService.get<string>('APP.DB.MONGODB_URL')
+      connection = await mongoose.connect(url);
+
+      isHealthy = true;
+    } catch (err) {
+      isHealthy = false;
+    }
+
+    const result = this.getStatus(key, isHealthy, {});
+
+    if (isHealthy) {
+      if (connection) await connection.disconnect();
+      return result;
+    }
+  }
+}
