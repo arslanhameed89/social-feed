a8ef720367ce2b96d8673c926d8ea40df3258b39
arsal <arsal.hameed@gmail.com>
2022-05-14 13:29:28 +0400

    add swagger fields

---
+++ src/v1/post/swagger/upload-request-body.ts
@@ -0,0 +1,21 @@
+export const UploadRequestBody = {
+  author: {
+    type: 'string'
+  },
+  title: {
+    type: 'string'
+  },
+  content: {
+    type: 'string'
+  },
+  category: {
+    type: 'string'
+  },
+  tags: {
+    type: 'array',
+    items: {
+      type: 'string'
+    },
+    description: "Tags in array []"
+  }
+}
