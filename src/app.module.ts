e0cb652a317e2ca25fc8461c45c8c8ccfe1e8bf0
arsal <arsal.hameed@gmail.com>
2022-05-16 16:49:53 +0400

    HTTP Request Logger Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,31 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import appConfig from './config/app.config'
-import { ConfigModule } from '@nestjs/config'
-import { ProvidersModule } from './providers/providers.module'
-import { CoreModule } from '@/core/core.module'
-import { V1Module } from '@/v1/v1.module'
-import { HealthModule } from '@/shared/health/health.module'
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import appConfig from "./config/app.config";
+import { ConfigModule } from "@nestjs/config";
+import { ProvidersModule } from "./providers/providers.module";
+import { CoreModule } from "@/core/core.module";
+import { V1Module } from "@/v1/v1.module";
+import { HealthModule } from "@/shared/health/health.module";
+import { RequestLoggerMiddleware } from "@/core/middleware/request-logger.middleware";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig],
-      envFilePath: ['.env']
+      envFilePath: [".env"],
     }),
     ProvidersModule,
     CoreModule,
     V1Module,
-    HealthModule
+    HealthModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(RequestLoggerMiddleware).forRoutes("*");
+  }
+}
